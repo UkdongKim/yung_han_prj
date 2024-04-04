6d76a9320703c8666eb842421b100e3f5eb79b3a
wook <edisen315@gmail.com>
2024-04-04 18:26:31 +0900

    setting files

---
+++ src/main/java/com/example/yung_han_prj/YungHanPrjApplication.java
@@ -0,0 +1,13 @@
+package com.example.yung_han_prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YungHanPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YungHanPrjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yung_han_prj/YungHanPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yung_han_prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YungHanPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
